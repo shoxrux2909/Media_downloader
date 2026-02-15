f79e24941c7cd58aafd3fdf7acc9eebcf6c4d9c9
shoxrux2909 <sh.abdullayev2003@gmail.com>
2026-02-15 22:37:12 +0500

    settings changed

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
